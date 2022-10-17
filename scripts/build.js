700e74635da9bb3197d8c18b7da65b2f698439b5
yintingwen <yintingwen@foxmail.com>
2022-10-18 00:36:08 +0800

    script build

---
+++ scripts/build.js
@@ -0,0 +1,28 @@
+import minimist from "minimist"
+// import execa from 'execa'
+import esbuild from "esbuild"
+import os from 'os'
+import path from "path"
+import fs from 'fs'
+
+const args = minimist(process.argv.slice(2))
+
+// if (args.length)
+
+function runParallel(target, task) {
+  const maxConcurrency = os.cpus().length
+  const executing  = []
+  const tasks = []
+
+  for (const item of target) {
+    const p = Promise.resolve().then(() => task(item))
+  }
+}
+
+async function build (target) {
+  const pkgDir = path.resolve( `packages/${target}`)
+  const pkg = fs.readFileSync( `${pkgDir}/package.json`, 'utf-8')
+  console.log(pkg);
+}
+
+build('request')
